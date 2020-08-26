d27ec659d9fd622e8ddf27f389245e7d9fd85206
Timon Schelling <timon.schelling@outlook.de>
2020-08-26 18:31:32 +0200

    -

--- cmd/save_git.go
+++ cmd/save_git.go
@@ -1,12 +1,9 @@
 package cmd
 
 import (
-	"fmt"
-	"log"
-	"os/exec"
-	"strings"
-
 	"github.com/spf13/cobra"
+
+	"github.com/timon-schelling/goschool/internal"
 )
 
 var SaveGit = &cobra.Command{
@@ -15,21 +12,6 @@ var SaveGit = &cobra.Command{
 	Short:   "use git to save changes",
 	Long:    "use git to save changes",
 	Run: func(cmd *cobra.Command, args []string) {
-		gitCmd := exec.Command("git", "rev-parse", "--show-toplevel")
-		gitCmdOut, err := gitCmd.Output()
-		if err != nil {
-			log.Fatal(err)
-		}
-		gitRepoRoot := strings.TrimSpace(string(gitCmdOut))
-		gitCmdAdd := exec.Command("git", "add", ".")
-		gitCmdAdd.Dir = gitRepoRoot
-		_ = gitCmdAdd.Run()
-		gitCmdCommit := exec.Command("git", "commit", "-m", "-")
-		gitCmdCommit.Dir = gitRepoRoot
-		_ = gitCmdCommit.Run()
-		gitCmdPush := exec.Command("git", "push")
-		gitCmdPush.Dir = gitRepoRoot
-		_ = gitCmdPush.Run()
-		fmt.Println("Done")
+		internal.SaveGit()
 	},
 }
